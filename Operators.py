6718b16c623ce26690e715d5e331bc7a2e72bfa6
bdushimi <lespharaons@hotmail.com>
2021-11-19 21:00:31 +0200

    Completes artimetic operations

---
+++ Operators.py
@@ -0,0 +1,45 @@
+#Operators
+
+
+
+#Numeric Operators
+"""
+
+Python supports different types of arithmetic operations 
+that can be performed on literal numbers, variables, or some combination. 
+
+The primary arithmetic operators are:
+
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+% for modulus (returns the remainder)
+** for exponentiation
+"""
+
+#TO-DO
+#Create a variable "result" that would store arithmetic operation on two numbers of your choice.
+#Try out an arithmetic operator you have not used
+#Print the "result" variable
+
+"""
+Precedence : refers to the order of operations
+
+In short, Python followed the order of operators outlined in BEDMAS.
+Read more here : https://www.programiz.com/python-programming/precedence-associativity
+
+An arithmetic expression containing different operators will be computed on the basis of operator precedence.
+Whenever operators have equal precedence, the expression is computed from the left side
+"""
+
+# Different precedence
+#print(10 - 3 * 2)  # Multiplication computed first, followed by subtraction
+
+# Same precedence
+#print(3 * 20 / 5)  # Multiplication computed first, followed by division
+#print(3 / 20 * 5)  # Division computed first, followed by multiplication
+
+#T0-D0
+#What should the code below output?
+#print((4 * 2) + 6 / 3 - 2)
--- README.md
+++
@@ -1,1 +0,0 @@
-# mbzuai-python-dsa
