8daf4c844db5020d453a47aadd6cd68ae7672750
bdushimi <lespharaons@hotmail.com>
2021-11-22 14:51:50 +0200

    exercies + assignment day 1

---
+++ warmup-challenges.py
@@ -0,0 +1,70 @@
+#Warmup Challenge
+
+# Challenge 1:
+# Write a program that prints out all the elements of the list "numbers" that are less than 5.
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+
+#Challenge 2:
+#Write a Python program which will return true if the two given integer values are equal or their sum or difference is 5.
+
+#Challenge 3:
+#Write a Python program to compute the distance between the points (x1, y1) and (x2, y2)
+
+#Challenge 4:
+# Define a function named count that takes a single parameter. 
+# The parameter is a string. The string will contain a single word divided into syllables by hyphens, such as these: "ba-na-na", "Data", "me-ta"
+# Your function should count the number of syllables and return it.
+# The call count("ho-tel") should return 2
+
+
+#Challenge 5:
+# Define a function named up_down that takes a single number as its parameter. 
+# Your function return a tuple containing two numbers; the first should be one lower than the parameter, and the second should be one higher.
+# For example, calling up_down(0) should return (-1, 1).
+
+
+#Challenge 6:
+#Define a function named consecutive_zeros that takes a single parameter, which is the string of zeros and ones
+#Your function should return the number of consecutive zeros in the string.
+#For instance, with this input "1001101000110", the function will return 3.
+
+#Challenge 7:
+# Write a Python program to check whether variable is integer or string or float.
+
+
+#Challenge 8:
+# Write a Python program to count and display the vowels of a given text.
+
+
+#Challenge 9:
+#Write a Python program to count the occurrences of each characher in a given sentence
+
+#Challenge 10:
+# Write a Python program which solve the equation: Go to the editor
+# ax+by=c
+# dx+ey=f
+# Print the values of x, y where a, b, c, d, e and f are given.
+
+
+
+
+
+
+#Assignment Day 1:
+#Problem statement:
+"""
+The Fibonacci sequence is a series of numbers where every number is the sum of the two numbers before it. The first two numbers are 0 and 1:
+
+You must write the fib() function which takes in a positive integer, n, and returns the n-th Fibonacci number. 
+Your function must use any of the loops.
+
+Sample input
+n=7
+
+Sample output
+8
+"""
+
+
+
