35c6264a29af2f92dc1ab09ee4c23d8f2dacdf8e
bdushimi <lespharaons@hotmail.com>
2021-11-23 01:42:36 +0200

    Adds some explanations on break, continue and pass keywords

--- Loops.py
+++ Loops.py
@@ -87,7 +87,6 @@ which means that the iterator will move forward by one step after each iteration
 #The while loop
 """
 The while loop keeps iterating over a certain set of operations as long as a certain condition holds True.
-
 It operates using the following logic: While this condition is true, keep the loop running
 """
 
@@ -126,4 +125,74 @@ while(x > 0):
 """
 
 
+#Nested for Loop
+"""
+A nested loop is a loop inside a loop.
+Use case : compare every element in a list with the rest of the elements in the list
+"""
 
+#Example :  print two elements whose sum is equal to a certain/given number
+# n = 50
+# num_list = [10, 4, 23, 6, 18, 27, 47]
+
+# for n1 in num_list:
+#     for n2 in num_list:  # Now we have two iterators
+#         if(n1 + n2 == n):
+#             print(n1, n2)
+
+
+# The break Keyword
+"""
+The break statement ends the loop.
+The break statement can be used inside a loop or in a function.
+
+Use case : 
+
+If you want to exit the loop because you have found what 
+you were looking for and don’t need to make any more computations in the loop
+"""
+
+# n = 50
+# num_list = [10, 4, 23, 6, 18, 27, 47]
+# found = False  # This bool will become true once a pair is found
+
+# for n1 in num_list:
+#     for n2 in num_list:
+#         if(n1 + n2 == n):
+#             found = True  # Set found to True
+#             break  # Break inner loop if a pair is found
+#     if found:
+#         print(n1, n2) # Print the pair
+#         break  # Break outer loop if a pair is found
+
+#The continue Keyword#
+"""
+The continue statement instructs a loop to continue to the next iteration. 
+Any code that follows the continue statement is not executed. 
+
+Unlike a break statement, a continue statement does not completely halt a loop
+"""
+
+# subjects = ["ML101", "DSA101", "WebDev101", "Statistics101"]
+
+# for subject in range(0, len(subjects)):
+#     if subjects[subject] == "DSA101":
+#         continue
+#         print(subjects[subject])
+#     else:
+#         print(subjects[subject])
+
+
+#The pass Keyword
+"""
+In all practical meaning, the pass statement does nothing to the code execution. 
+It can be used to represent an area of code that needs to be written. 
+Hence, it is simply there to assist you when 
+you haven’t written a piece of code but still need your entire program to execute.
+
+"""
+# num_list = list(range(20))
+# for num in num_list:
+#     pass # You can write code here later on
+
+# print(num_list) 
