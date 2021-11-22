71410bbb054640830817fce429d2bf438beeca38
bdushimi <lespharaons@hotmail.com>
2021-11-23 01:12:12 +0200

    Adds challenges solutions & Assignment

--- solutions.py
+++ solutions.py
@@ -1,4 +1,6 @@
 import math
+
+
 # Challenge 1
 # Write a program that prints out all the elements of the list "numbers" that are less than 5.
 
@@ -93,17 +95,129 @@ import math
 
 #Challenge 7:
 # Write a Python program to check whether variable is integer or string or float.
-def checkType(data):
-    if "str" in type(data):
-        print(data, "is a string")
-    if "int" in type(data):
-        print(data, "is an integer")
-    if "float" in type(data):
-        print(data, "is a float")
+# def checkType(data):
 
-checkType("2")
-checkType(2)
-checkType(2.0)
+#     if "str" in str(type(data)):
+#         print(data, "is a string")
+#     if "int" in str(type(data)):
+#         print(data, "is an integer")
+#     if "float" in str(type(data)):
+#         print(data, "is a float")
+
+# checkType("2")
+# checkType(2)
+# checkType(2.0)
+
+#Challenge 8:
+# Write a Python program to count and display the vowels of a given text.
+
+# def countVowels(str):
+#     vowelsCount=0
+#     for char in str:
+#         if char == "o" or char == "i" or char == "e" or char == "u" or char == "a":
+#             vowelsCount += 1
+#     return vowelsCount
+    
+# print(countVowels("Judge James"))
+# print(countVowels("dhfgty"))
+
+
+#Challenge 9:
+#Write a Python program to count the occurrences of each characher in a given sentence
+
+# def countCharFreq(str):
+#     #create an dictionary
+#     nums_dic = {}
+#     for char in str:
+#         char = char.lower()
+#         if char.rstrip() != "" and not nums_dic.get(char):
+#             nums_dic[char]=1
+#         elif nums_dic.get(char) :
+#              nums_dic[char] += 1
+#     print(nums_dic)
+    
+# countCharFreq("Data")
+# countCharFreq("God is Good and Amazing")
+
+
+#Challenge 10:
+# Write a Python program which solve the equation:
+# ax+by=c
+# dx+ey=f
+# Print the values of x, y where a, b, c, d, e and f are given.
+
+#Solved using Determinants approach
+# def resolve(a,b,c,d,e,f):
+    
+#     #The value of this determinant is found by finding the difference between 
+#     # the diagonally down product and the diagonally up product
+#     D = a*e - b*d
+    
+#     if D != 0:
+        
+#         # The x‐determinant or Dx is formed by taking the constant terms 
+#         # from the system and placing them in the x‐coefficient positions and retaining the y‐coefficients.
+        
+#         Dx = (c*e - b*f)
+#         x = Dx / D
+        
+#         # The y-determinant or Dy is formed by taking the constant terms 
+#         # from the system and placing them in the y‐coefficient positions and retaining the x‐coefficients.
+
+#         Dy = (a*f - c*d)
+#         y = Dy / D
+        
+#         print(x, y)
+
+# resolve(3,2,10,2,-2,5)
+
+#Assignment Day 1:
+#Problem statement:
+"""
+The Fibonacci sequence is a series of numbers where every number is the sum of the two numbers before it. 
+The first two numbers are 0 and 1:
+
+You must write the fib() function which takes in a positive integer, n, and returns the n-th Fibonacci number. 
+Your function must use any of the loops (for loop or while loop).
+
+Sample input
+n=7
+
+Sample output
+8
+"""
+
+# def fib(n):
+#     # The first and second values will always be fixed
+#     first = 0
+#     second = 1
+
+#     if n < 1:
+#         return -1
+
+#     if n == 1:
+#         return first
+
+#     if n == 2:
+#         return second
+
+#     count = 3  # Starting from 3 because we already know the first two values
+#     while count <= n:
+#         fib_n = first + second
+#         first = second
+#         second = fib_n
+#         count += 1  # Increment count in each iteration
+#     return fib_n
+
+
+# n = 3
+# print(fib(n))
+
+
+
+
+
+
 
 
 
