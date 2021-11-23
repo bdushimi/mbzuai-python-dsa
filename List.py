57e2728fe259a68784b861eab8d2fe812c44ec8e
bdushimi <lespharaons@hotmail.com>
2021-11-23 02:24:33 +0200

    Adds basics of list manipulation

---
+++ List.py
@@ -0,0 +1,38 @@
+
+#Creating a list
+dogs = ["Spot", "Max", "Sam", "Charlie", "Cooper", "Duke", "Bear", "Buddy", "Milo", "Murphy"]
+# print(dogs)
+
+# Common list operations
+
+# Add elements to the end of the list using the append() method
+# dogs.append("Wrigley")
+# print(dogs)
+
+# Add elements to a given index location within the list using insert() method
+# dogs.insert(1, "Wrigley")
+# print(dogs)
+
+
+#Access a specific element
+# x = dogs[0]
+# print(x)
+
+#Change a specific element
+# dogs[0] = "Jack"
+# print(dogs)
+
+#Accessing a range of values by clicing Items in a Python Array
+print(dogs[1:4])
+
+#Remove elements, with the the pop(), you can remove an element at a specified position
+# dogs.pop(9)
+# print(dogs)
+
+# use the remove() method to remove a specified element from an array
+# The list's remove() method only removes the first occurrence of the specified value.
+
+# dogs.remove("Duke")
+# print(dogs)
+
+# More of List built-in functions here : https://www.w3schools.com/python/python_arrays.asp
