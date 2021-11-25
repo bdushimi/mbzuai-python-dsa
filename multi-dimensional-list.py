38adfa52dbeef6e856f8b4e15b836c4f338aec92
bdushimi <lespharaons@hotmail.com>
2021-11-25 08:45:34 +0200

    Completes multi-dimensional list

--- multi-dimensional-list.py
+++ multi-dimensional-list.py
@@ -18,10 +18,83 @@ List = [ [1, 2], [2,5], [5,1] ]
 
 
 #For example:
-print(List[0]) # [1, 2]
+# print(List[0]) # [1, 2]
 
 # The elements of the inner list can be accessed using the index of the inner list.
 # Add one more square bracket to access its elements 
 
-print(List[0][0]) # 1
+# print(List[0][0]) # 1
+
+# Access multi-dimensional list with the help of loop.
+# for record in List:
+#     print(record)
+
+# Access multidimensional list using square brackets and for loop
+
+# for i in range(len(List)) : # len(List) is the length of the outer list 
+#     for j in range(len(List[i])) :  # len(List[i]) is the length of the inner list
+#         print(List[i][j], end=" ") 
+#     print()
+
+
+# Methods on Multidimensional lists
+
+# Add a sublist to a multidimensional list
+# List.append([20, 25])
+# print(List)
+
+
+# Extend a sublist 
+# List[0].extend([11, 33])
+# print(List)
+
+
+# Practical Example : Matrices
+
+# In Python, we can implement a matrix as a nested list (list inside a list). 
+# We can treat each element as a row of the matrix.
+
+# Program to add two matrices using nested loop
+
+# X = [[6,4,-1],
+#     [14 ,0,6],
+#     [3 ,8,1]]
+
+# Y = [[2,8,1],
+#     [4,5,3],
+#     [9,5,1]]
+
+# result = [[0,0,0],
+#          [0,0,0],
+#          [0,0,0]]
+
+# for i in range(len(X)):
+#    for j in range(len(X[0])):
+#        result[i][j] = X[i][j] + Y[i][j]
+
+# for r in result:
+#    print(r)
+
+# Program to transpose a matrix using a nested loop
+# Transpose of a matrix is obtained by swapping the rows and columns of the matrix.
+
+
+# X = [[6,2],
+#     [8 ,4],
+#     [9 ,0]]
+
+# result = [[0,0,0],
+#          [0,0,0]]
+
+# # iterate through rows
+# for i in range(len(X)):
+#    # iterate through columns
+#    for j in range(len(X[0])):
+#        result[j][i] = X[i][j]
+
+# for r in result:
+#    print(r)
+
+
+ 
 
