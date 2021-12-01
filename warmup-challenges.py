1de7a8690ccaa2017c686ad35673f661b36872d6
DUSHIMIMANA Bernard <35345818+bdushimi@users.noreply.github.com>
2021-12-01 12:46:47 +0200

    Update warmup-challenges.py

--- warmup-challenges.py
+++ warmup-challenges.py
@@ -51,20 +51,7 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 
 
-#Assignment Day 1:
-#Problem statement:
-"""
-The Fibonacci sequence is a series of numbers where every number is the sum of the two numbers before it. The first two numbers are 0 and 1:
 
-You must write the fib() function which takes in a positive integer, n, and returns the n-th Fibonacci number. 
-Your function must use any of the loops (for loop or while loop).
-
-Sample input
-n=7
-
-Sample output
-8
-"""
 
 
 
