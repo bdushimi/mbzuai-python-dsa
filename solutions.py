a8fd000995d94ae09d9c17385f8f84bf0a899b0c
bdushimi <lespharaons@hotmail.com>
2021-12-04 15:18:59 +0200

    Adds solution 6

--- solutions.py
+++ solutions.py
@@ -335,8 +335,31 @@ Sample output
 #             second_max = item
 #     return second_max
 
-
 # print(find_second_maximum([9, 2, 3, 6]))
+
+
+# Challenge 6
+# Implement a function right_rotate(lst, k) which will rotate the given list by k. This means that the right-most elements will appear at the left-most position in the list and so on. You only have to rotate the list by one element at a time.
+# Input#
+# A list and a positive number by which to rotate that list
+# Output:#
+# The given list rotated by k elements
+# Sample Input#
+# lst = [10,20,30,40,50]
+# k = 3
+# Sample Output#
+# lst = [30,40,50,10,20]
+
+# def right_rotate(lst, k):
+#     for i in range(k):
+#         last_element = lst.pop()
+#         lst.insert(0, last_element)
+#     return lst
+# print(right_rotate([10,20,30,40,50], 3))
+
+
+  
+
 
 
 
